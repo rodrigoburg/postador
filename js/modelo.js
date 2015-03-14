918aa612ee9a3de66ea7469f197c057ec594328c
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2015-03-14 00:32:15 -0300

    cor especifica e ordem da legenda

--- js/modelo.js
+++ js/modelo.js
@@ -1,3 +1,6 @@
+var chave_planilha = "1P3LnfA-uA4EVCVHiPfqabLI-oCRHNf4Kb9UXoOk2Cbo" //link do modelo
+
+
 //var chave_planilha = "12uJCNURJ-8AinCrLfzi24DKHhj6aFGMota46Z9hFkh0" //link do modelo
 /**
  * Created by rodrigoburg on 09/02/15.
@@ -117,6 +120,8 @@ var cria_tags = function (callback) {
                         tipo_y:item["tipo_y"],
                         serie:item["serie"],
                         legenda:item["legenda"],
+                        ordem_legenda:item["ordem_legenda"],
+                        cores:item["cores"],
                         outros:item["outros"]
                     },
                     contador_graficos++
@@ -148,7 +153,7 @@ var cria_tags = function (callback) {
     if (callback) callback()
 
 }
-
+window.grafico = null
 function desenha_grafico(item) {
     baixa_planilha_dados(item["posicao"], function (dados) {
         var tipo_grafico = item["tipo_grafico"],
@@ -159,8 +164,23 @@ function desenha_grafico(item) {
             tipo_y = item["tipo_y"],
             serie = item["serie"],
             legenda = item["legenda"],
+            ordem_legenda = item["ordem_legenda"].split(","),
             index = item["contador"],
+            cores = item["cores"].split(","),
             outros = (item["outros"]) ? item["outros"].split(",") : null
+
+        var cores_default = [
+            "#A11217",
+            "#BA007C",
+            "#5E196F",
+            "#00408F",
+            "#007CC0",
+            "#009493",
+            "#00602D",
+            "#A3BD31",
+            "#E9BC00",
+            "#634600"
+        ]
 
         var svg = dimple.newSvg("#grafico"+index,width,height)
         var myChart = new dimple.chart(svg, dados);
@@ -171,7 +191,10 @@ function desenha_grafico(item) {
             var x = myChart.addMeasureAxis("x", x);
         } else if (tipo_x == "categorico" || tipo_x == "categórico" || tipo_x == "categoria" || tipo_x == "categorica" || tipo_x == "categórica") {
             var x = myChart.addCategoryAxis("x", x);
+        } else if (tipo_x == "time" || tipo_x == "tempo") {
+             var x = myChart.addTimeAxis("x",x,"%y","%y")
         }
+
 
         if (tipo_y == "valor") {
             var y = myChart.addMeasureAxis("y", y);
@@ -190,33 +213,63 @@ function desenha_grafico(item) {
             lista_series = serie.split(",")
         }
         if (tipo_grafico == "bar" || tipo_grafico == "barra") {
-            myChart.addSeries(lista_series, dimple.plot.bar);
+            var s = myChart.addSeries(lista_series, dimple.plot.bar);
         } else if (tipo_grafico == "linha" || tipo_grafico == "line") {
-            myChart.addSeries(lista_series, dimple.plot.line);
+            var s = myChart.addSeries(lista_series, dimple.plot.line);
         } else if (tipo_grafico == "dispersão" || tipo_grafico == "dispersao" || tipo_grafico == "scatter") {
-            myChart.addSeries(lista_series, dimple.plot.bubble);
+            var s = myChart.addSeries(lista_series, dimple.plot.bubble);
+        }
+
+        if (outros) {
+            if (outros.indexOf("interpolar_cardinal" > -1)) {
+                s.interpolation = "cardinal"
+            }
+        }
+
+
+        if (cores) {
+            for (c in cores) {
+                var temp = cores[c].split("=")
+                myChart.assignColor(temp[0],cores_default[temp[1]])
+            }
+
+        } else {
+            var cores = []
+            for (c in cores_default) {
+                cores.append(new dimple.color(cores_default[c]))
+            }
+            myChart.defaultColors = shuffle(cores);
         }
 
         //series.lineWeight = 1.8;
         if (legenda == "sim" || legenda == "Sim" || legenda == "SIM") {
-            myChart.addLegend(margins.left+10,margins.top-20, width-margins.right, 200)
+            legenda = myChart.addLegend(margins.left+10,margins.top-20, width-margins.right, 200)
+            if (ordem_legenda) {
+                legenda._getEntries = function () {
+                    var orderedValues = ordem_legenda;
+                    var entries = [];
+                    orderedValues.forEach(function (v) {
+                        v = v.trim()
+                        entries.push(
+                            {
+                                key: v,
+                                fill: myChart.getColor(v).fill,
+                                stroke: myChart.getColor(v).stroke,
+                                opacity: myChart.getColor(v).opacity,
+                                series: s,
+                                aggField: [v]
+                            }
+                        );
+                    }, this);
+
+                    return entries;
+                };
+
+            }
         }
 
-        var colors = [
-            new dimple.color("#8F7D59"),
-            new dimple.color("#8F5959"),
-            new dimple.color("#8F597D"),
-            new dimple.color("#8F598F"),
-            new dimple.color("#7D598F"),
-            new dimple.color("#59598F"),
-            new dimple.color("#598F7D"),
-            new dimple.color("#598F59")
-        ]
-
-        myChart.defaultColors = shuffle(colors);
-
         myChart.draw();
-
+        window.grafico = myChart
         console.log($("body").height()+30)
 
         if (titulo_grafico)
